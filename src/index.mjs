fbd02869c9f73b268617561e71575672fca12983
Markus Felten <Markus.Felten@gmx.de>
2023-01-28 13:47:46 +0100

    fix: esm

--- src/index.mjs
+++ src/index.mjs
@@ -1,15 +1,14 @@
-/*
 import { Router } from "itty-router";
 import { error, json, missing } from "itty-router-extras";
 import { createCors } from "itty-cors";
 
-const { preflight, corsify } = createCors();
+//const { preflight, corsify } = createCors();
+
 
 const router = Router();
 
-// register v2 API plus all routes
 router
-  .all("*", preflight) // handle CORS preflight/OPTIONS requests
+//  .all("*", preflight) // handle CORS preflight/OPTIONS requests
   .get("/version", () => json({ version: "0.1.0" })) // GET release version
   .get("/stuff", () => json(["foo", "bar", "baz"])) // GET some other random stuff
   .all("*", () => missing("Are you sure about that?")); // 404 for all else
@@ -19,9 +18,9 @@ export default {
     router
       .handle(...args)
       .catch(err => error(500, err.stack))
-      .then(corsify)
+//      .then(corsify)
 };
-*/
+
 
 addEventListener("fetch", event => {
   event.respondWith(handleRequest(event.request));
@@ -30,7 +29,7 @@ addEventListener("fetch", event => {
 /**
  * Respond to the request
  * @param {Request} request
- */
+ */ 
 async function handleRequest(request) {
   const params = new URLSearchParams(request.url.replace(/^[^\?]+\?/, ""));
 
