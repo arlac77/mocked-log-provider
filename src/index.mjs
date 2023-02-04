bd58c3a31c8d64d69f7722fa68d32f5f0cbab1e7
Markus Felten <Markus.Felten@gmx.de>
2023-02-04 23:38:28 +0100

    fix: add cors

--- src/index.mjs
+++ src/index.mjs
@@ -2,20 +2,22 @@ import { Router } from "itty-router";
 import { error, json, missing } from "itty-router-extras";
 import { createCors } from "itty-cors";
 
-//const { preflight, corsify } = createCors();
+const { preflight, corsify } = createCors({ allowOrigin: "*" });
 
 const router = Router();
 
 router
-  //  .all("*", preflight) // handle CORS preflight/OPTIONS requests
+  .all("*", preflight) // handle CORS preflight/OPTIONS requests
   .get("/version", () => json({ version: "0.1.0" }))
   .get("/", getLog)
   .all("*", () => missing("Are you sure about that?"));
 
 export default {
   fetch(...args) {
-    return router.handle(...args).catch(err => error(500, err.stack));
-    //      .then(corsify)
+    return router
+      .handle(...args)
+      .catch(err => error(500, err.stack))
+      .then(corsify);
   }
 };
 
