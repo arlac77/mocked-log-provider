b79d58f80f610d36c2a168a76cd3f7522311cb62
Markus Felten <Markus.Felten@gmx.de>
2024-05-18 15:39:11 +0100

    fix: init order

--- src/cli.mjs
+++ src/cli.mjs
@@ -2,6 +2,30 @@ import impl from "./index.mjs";
 
 import { createServer } from "node:http";
 
+/* See SD_LISTEN_FDS_START from
+ * http://cgit.freedesktop.org/systemd/systemd/tree/src/systemd/sd-daemon.h */
+ const firstSystemdFD = 3;
+ let nextIndex = 0;
+ 
+ function systemdSocket(index) {
+   if (arguments.length < 1) {
+     index = nextIndex++;
+   }
+ 
+   if (!process.env.LISTEN_FDS) {
+     return undefined;
+   }
+ 
+   const listenFDs = parseInt(process.env.LISTEN_FDS, 10);
+   if (listenFDs < nextIndex) {
+     return undefined;
+   }
+ 
+   return {
+     fd: firstSystemdFD + index
+   };
+ }
+ 
 const server = createServer((req, res) => {
   impl.fetch(req, res);
 });
@@ -21,26 +45,3 @@ try {
   console.error(e);
 }
 
-/* See SD_LISTEN_FDS_START from
- * http://cgit.freedesktop.org/systemd/systemd/tree/src/systemd/sd-daemon.h */
-const firstSystemdFD = 3;
-let nextIndex = 0;
-
-function systemdSocket(index) {
-  if (arguments.length < 1) {
-    index = nextIndex++;
-  }
-
-  if (!process.env.LISTEN_FDS) {
-    return undefined;
-  }
-
-  const listenFDs = parseInt(process.env.LISTEN_FDS, 10);
-  if (listenFDs < nextIndex) {
-    return undefined;
-  }
-
-  return {
-    fd: firstSystemdFD + index
-  };
-}
