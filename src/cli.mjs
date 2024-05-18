daabc7e04956b24058c2056aeb6368a0e531074a
Markus Felten <Markus.Felten@gmx.de>
2024-05-18 15:30:14 +0100

    feat: systemd integration

--- src/cli.mjs
+++ src/cli.mjs
@@ -6,6 +6,41 @@ const server = createServer((req, res) => {
   impl.fetch(req, res);
 });
 
-let port = "/run/mocked-log-provider/http/socket";
+try {
+  const sd = await import("sd-daemon");
+  sd.notify("STATUS=starting");
 
-server.listen(port);
+  server.listen(systemdSocket(), error => {
+    if (error) {
+      sd.notify("READY=1\nERRNO=" + error);
+    } else {
+      sd.notify("READY=1\nSTATUS=running");
+    }
+  });
+} catch (e) {
+  console.error(e);
+}
+
+/* See SD_LISTEN_FDS_START from
+ * http://cgit.freedesktop.org/systemd/systemd/tree/src/systemd/sd-daemon.h */
+const firstSystemdFD = 3;
+let nextIndex = 0;
+
+function systemdSocket(index) {
+  if (arguments.length < 1) {
+    index = nextIndex++;
+  }
+
+  if (!process.env.LISTEN_FDS) {
+    return undefined;
+  }
+
+  const listenFDs = parseInt(process.env.LISTEN_FDS, 10);
+  if (listenFDs < nextIndex) {
+    return undefined;
+  }
+
+  return {
+    fd: firstSystemdFD + index
+  };
+}
