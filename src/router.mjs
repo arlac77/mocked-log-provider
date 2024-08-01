8b998cb34e0b7f84f9648a1ac112d9c38b02bc8d
Markus Felten <Markus.Felten@gmx.de>
2024-08-01 12:15:40 +0100

    fix: report real version

--- src/router.mjs
+++ src/router.mjs
@@ -2,6 +2,7 @@ import { AutoRouter } from "itty-router";
 import { json, missing } from "itty-router-extras";
 import { createCors } from "itty-cors";
 import { getLog } from "./log.mjs";
+import pkg from "../package.json" with { type: "json" };
 
 const { preflight, corsify } = createCors({ allowOrigin: "*" });
 
@@ -9,7 +10,7 @@ const router = AutoRouter();
 
 router
   .all("*", preflight)
-  .get("/version", () => json({ version: "0.4.0" }))
+  .get("/version", () => json({ version: pkg.version }))
   .get("/", getLog)
   .all("*", () => missing("Are you sure about that?"));
 
