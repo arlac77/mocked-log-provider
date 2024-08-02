bc9bae1da226696eef966c253817ab4f32caf0db
Markus Felten <Markus.Felten@gmx.de>
2024-08-02 18:27:25 +0100

    feat: log + report exception

--- src/log.mjs
+++ src/log.mjs
@@ -3,22 +3,23 @@
  * @param {Request} request
  */
 export async function getLog(request) {
-  const params = new URLSearchParams(request.url.replace(/^[^\?]+\?/, ""));
-  const param = (attribute, d = 0) => {
-    const value = params.get(attribute);
-    return (value !== null && parseInt(value)) || d;
-  };
+  try {
+    const params = new URLSearchParams(request.url.replace(/^[^\?]+\?/, ""));
+    const param = (attribute, d = 0) => {
+      const value = params.get(attribute);
+      return (value !== null && parseInt(value)) || d;
+    };
 
-  let cursor = param("cursor", 0);
-  const offset = param("offset", 0);
-  let number = param("number", 10);
-  let delay = param("delay", 200);
+    let cursor = param("cursor", 0);
+    const offset = param("offset", 0);
+    let number = param("number", 10);
+    let delay = param("delay", 200);
 
-  //console.log(cursor,offset,number,delay);
+    //console.log(cursor,offset,number,delay);
 
-  const te = new TextEncoder();
+    const te = new TextEncoder();
 
-  /*
+    /*
   const readableStream = new ReadableStream({
     async pull(controller) {
       const iv = setInterval(async () => {
@@ -34,21 +35,28 @@ export async function getLog(request) {
 
   const readable = readableStream.getReader();
   */
-  const { readable, writable } = new TransformStream();
+    const { readable, writable } = new TransformStream();
 
-  const writer = writable.getWriter();
+    const writer = writable.getWriter();
 
-  const iv = setInterval(async () => {
-    writer.write(te.encode(`line ${offset + cursor++}\n`));
-    if (number-- <= 0) {
-      clearInterval(iv);
-      await writer.ready;
-      await writer.close();
-    }
-  }, delay);
+    const iv = setInterval(async () => {
+      writer.write(te.encode(`line ${offset + cursor++}\n`));
+      if (number-- <= 0) {
+        clearInterval(iv);
+        await writer.ready;
+        await writer.close();
+      }
+    }, delay);
 
-  return new Response(readable, {
-    headers: { "Content-Type": "text/plain" },
-    status: 200
-  });
+    return new Response(readable, {
+      headers: { "Content-Type": "text/plain" },
+      status: 200
+    });
+  } catch (e) {
+    console.error(e);
+    return new Response(e.message, {
+      headers: { "Content-Type": "text/plain" },
+      status: 500
+    });
+  }
 }
