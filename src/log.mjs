cc5a3f59a36edebcd0ef25265584e916c412f047
Markus Felten <Markus.Felten@gmx.de>
2024-08-03 12:43:50 +0100

    fix: prevent exception

--- src/log.mjs
+++ src/log.mjs
@@ -15,26 +15,8 @@ export async function getLog(request) {
     let number = param("number", 10);
     let delay = param("delay", 200);
 
-    //console.log(cursor,offset,number,delay);
-
     const te = new TextEncoder();
 
-    /*
-  const readableStream = new ReadableStream({
-    async pull(controller) {
-      const iv = setInterval(async () => {
-        if (number-- <= 0) {
-          clearInterval(iv);
-          controller.close();
-        } else {
-          controller.enqueue(te.encode(`line ${offset + cursor++}\n`));
-        }  
-      }, delay);
-    }
-  });
-
-  const readable = readableStream.getReader();
-  */
     const { readable, writable } = new TransformStream();
 
     const writer = writable.getWriter();
@@ -43,7 +25,6 @@ export async function getLog(request) {
       writer.write(te.encode(`line ${offset + cursor++}\n`));
       if (number-- <= 0) {
         clearInterval(iv);
-        await writer.ready;
         await writer.close();
       }
     }, delay);
